653240380911cf3c9427767ab8c7386c1926614f
Prity <prity12148@gmail.com>
2024-07-15 16:22:50 +0530

    final

--- frontend/src/components/layout/Footer.js
+++ frontend/src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div style={{height:'10vh', backgroundColor: "#FB6F90",display:"flex",alignItems:"center", justifyContent:"center",fontWeight:"bold"}}>
-        <p>Copyright</p>
+        <p>Copyright @ PK 2024.  All Rights Reserved.</p>
     </div>
   )
 }
